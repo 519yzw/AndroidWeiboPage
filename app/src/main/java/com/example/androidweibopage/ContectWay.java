ad4fc1a51a5f5e1450eff800ac8ae6bba3dcb9af
519yzw <519442133@qq,com>
2019-05-14 17:30:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidweibopage/ContectWay.java
@@ -0,0 +1,13 @@
+package com.example.androidweibopage;
+/*
+ * 请不要更改此文件中任何内容(如果报错,请在复选报错代码，用感叹号设置为Level 8)
+ * 需要取出数据库数据的变量请放入getData()
+ * 需要存放数据库数据的变量请放入setData()
+ * 此接口仅用于将符合以上要求的代码放入对应方法中,其他变量请正常操作
+ * author:yzw
+ * time:2019/5/14
+ */
+public interface ContectWay {
+    public static void getData(){}
+    public static void setData(){}
+}
---
+++ app/src/main/java/com/example/androidweibopage/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.androidweibopage;
+/*
+ * 参照ContectWay进行与服务器端交互操作
+ * author:yzw
+ * time:2019/5/12
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements ContectWay{
+
+    public static void getData(){}
+    public static void setData(){}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
